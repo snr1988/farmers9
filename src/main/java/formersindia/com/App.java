77b29b655ac109ef03bf2c9c9472ebf887c2a2bf
suryach <suryanarayana516@gmail.com>
2021-04-08 10:36:13 +0530

    commit

---
+++ src/main/java/formersindia/com/App.java
@@ -0,0 +1,13 @@
+package formersindia.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
